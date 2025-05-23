a9624770df20b0a7afd01f7f084a84526b26ccbb
deepesh-sr <220714100078@cutm.ac.in>
2025-05-23 16:59:44 +0530

    Mesh added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { OrbitControls } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+const RotatingCube = () =>{
+  const meshRef = useRef();
+  useFrame(
+    ()=>{
+      if(meshRef.current){
+        meshRef.current.rotation.y +=0.01;
+        // meshRef.current.rotation.z +=0.01;
+      }
+    }
+  )
+  return(
+    <mesh ref={meshRef} >
+        <cylinderGeometry args={[1,1,2]}/>
+        <meshPhongMaterial color="purple" emissive="black"/>
+    </mesh>
   )
 }
 
-export default App
+const App = () => {
+  return (
+    <Canvas style={{backgroundColor:"#F2F2F2", width:"100vw", height:"100vh"}}  camera={{position:[0,0,10]}}>
+      <OrbitControls enableZoom enableDamping enablePan/>
+      <ambientLight intensity={0.6}/>
+      <directionalLight position={[1,1,2]} intensity={10} color={0x9CDBA6}/>
+    <RotatingCube/>
+    </Canvas>
+  )
+}
+
+export default App;
