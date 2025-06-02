eeee5f7c930490e9b97887248f30aafbc6158cd1
deepesh-sr <220714100078@cutm.ac.in>
2025-06-02 23:41:30 +0530

    model added :)

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,16 @@ import { Canvas, useFrame } from "@react-three/fiber";
 
 import { RotatingCube } from "./components/RotatingCube.jsx";
 import { Navbar } from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
 
 
 
 const App = () => {
-  return (<>
-     {/* <Canvas style={{backgroundColor:"#F2F2F2", width:"100vw", height:"100vh"}}  camera={{position:[0,0,10]}}>
-      <OrbitControls enableZoom enableDamping enablePan/>
-      <ambientLight intensity={0.6}/>
-      <directionalLight position={[1,1,2]} intensity={10} color={0x9CDBA6}/>
-    <RotatingCube/>
-    </Canvas>  */}
+  return (
+    <main className="max-w-7xl mx-auto">
     <Navbar/>
-    </>
+    <Hero/>
+    </main>
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,29 @@
-import React from "react";
+import { PerspectiveCamera } from "@react-three/drei";
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { CanvasLoader } from "./CanvasLoader";
 
 const Hero = () => {
     return (
         <section
             className="min-h-screen w-full flex flex-col relative"
         >
-        <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 sm:px-10 px-5 gap-3">
-            <p className="sm:text-3xl text-2xl font-medium text-black text-center font-">
-               Hi , I am Deepesh <span>🦀</span>
-            </p>
-            <p className="text-center xl:text-6xl md:text-5xl sm:text-4xl text-3xl font-generalsans font-black !leading-normal">
-        I am a Rust Developer 
-            </p>
-        </div>
-        <div className="w-full h-full absolute inset-0">
+            <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 sm:px-10 px-5 gap-3">
+                <p className="sm:text-3xl text-2xl font-medium text-black text-center font-">
+                    Hi , I am Deepesh <span>🦀</span>
+                </p>
+                <p className="text-center xl:text-6xl md:text-5xl sm:text-4xl text-3xl font-generalsans font-black !leading-normal">
+                    I am a Rust Developer
+                </p>
+            </div>
+            <div className="w-full h-full absolute inset-0">
+                <Canvas className='w-full h-full'>
+                    <Suspense fallback={<CanvasLoader/>}>
 
-        </div>
+                        <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+                    </Suspense>
+                </Canvas>
+            </div>
         </section>
     )
 }
